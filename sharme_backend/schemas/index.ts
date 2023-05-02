b5c2ae42610929b83b2f7cc4090fdcad440bca11
Zain Ali <ZainSoftwear11@gmail.com>
2023-05-02 15:49:39 +0500

    Modified

--- sharme_backend/schemas/index.ts
+++ sharme_backend/schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import User from './User';
+import Comment from './Comment';
+import Save from './Save';
+import Pin from './Pin';
+import PostedBy from './PostedBy';
+export const schemaTypes = [
+    User, Save, Comment, Pin, PostedBy
+];
